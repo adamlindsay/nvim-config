19498a37ff3bd16c0424db80e7f2f77a286602a0
Adam Lindsay <adam.lindsay@MacBook-Pro.local>
2026-06-22 10:13:06 -0400

    feat: bootstrap lazy.nvim with options and keymaps

---
+++ lua/config/options.lua
@@ -0,0 +1,17 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.signcolumn = "yes"
+opt.termguicolors = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.smartindent = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.splitright = true
+opt.splitbelow = true
+opt.scrolloff = 5
+opt.undofile = true
