39bb3b3e197484b982b088464139918ee537123a
Adam Lindsay <adam.lindsay@MacBook-Pro.local>
2026-06-22 10:21:39 -0400

    feat(plugins): add LSP with mason, ruby_lsp, ts_ls, bashls

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "Saghen/blink.cmp",
+    },
+    config = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+      vim.lsp.config("*", { capabilities = capabilities })
+
+      require("mason-lspconfig").setup({
+        ensure_installed = { "ruby_lsp", "ts_ls", "bashls" },
+        automatic_installation = true,
+      })
+    end,
+  },
+}
