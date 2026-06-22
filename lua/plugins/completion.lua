68ce84d9b4a41c90e21b212fc6ec715dfbe1d3b3
Adam Lindsay <adam.lindsay@MacBook-Pro.local>
2026-06-22 10:19:55 -0400

    feat(plugins): add blink.cmp completion

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,13 @@
+return {
+  "Saghen/blink.cmp",
+  version = "*",
+  opts = {
+    keymap = { preset = "default" },
+    sources = {
+      default = { "lsp", "path", "buffer" },
+    },
+    completion = {
+      documentation = { auto_show = true },
+    },
+  },
+}
