461474093bda8f4e4a2efae2dbe2917d86a1afd9
Adam Lindsay <adam.lindsay@MacBook-Pro.local>
2026-06-22 10:23:27 -0400

    feat(plugins): add conform.nvim format-on-save

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,19 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  opts = {
+    formatters_by_ft = {
+      ruby = { "rubocop" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      sh = { "shfmt" },
+      bash = { "shfmt" },
+    },
+    format_on_save = {
+      timeout_ms = 1000,
+      lsp_fallback = true,
+    },
+  },
+}
